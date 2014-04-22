21c8562aa52e595fefa8d48ddaa0d6598fc45ec4
Dan Bouffard <bouffard.daniel25@gmail.com>
2014-04-22 18:03:40 +0200

    Start the churn test script

--- experiments/genfile.py
+++ experiments/genfile.py
@@ -3,11 +3,11 @@
 import os
 import sys
 
-def gen_file(numMs, numGs):
+def gen_file(numMs, numGs, filename):
     oneMBString = '0' * 1024 * 1024
-    f = open(fname, 'w')
+    f = open(filename, 'w')
     
-    print("Generating " + fname + " (this might take a while)")
+    print("Generating " + filename + " (this might take a while)")
     
     if numGs == 0:
         for mb in range(0, numMs): # Number of MB
@@ -22,23 +22,24 @@ if len(sys.argv) < 3 or (sys.argv[1] != '1M'  and sys.argv[1] != '10M' and sys.a
     print("Usage: " + os.path.basename(__file__) + " [1M|10M|100M|1G|10G] output_file")
     sys.exit()
 
-fsize = sys.argv[1]
-fname = sys.argv[2]
-
-if fsize == '1M':
-    numMs = 1
-    numGs = 0
-elif fsize == '10M':
-    numMs = 10
-    numGs = 0
-elif fsize == '100M':
-    numMs = 100
-    numGs = 0
-elif fsize == '1G':
-    numMs = 1024
-    numGs = 1
-else:
-    numMs = 1024
-    numGs = 10
-
-gen_file(numMs, numGs)
+if __name__ == '__main__':
+    fsize = sys.argv[1]
+    fname = sys.argv[2]
+    
+    if fsize == '1M':
+        numMs = 1
+        numGs = 0
+    elif fsize == '10M':
+        numMs = 10
+        numGs = 0
+    elif fsize == '100M':
+        numMs = 100
+        numGs = 0
+    elif fsize == '1G':
+        numMs = 1024
+        numGs = 1
+    else:
+        numMs = 1024
+        numGs = 10
+        
+    gen_file(numMs, numGs, fname)
